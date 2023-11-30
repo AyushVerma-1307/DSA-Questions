d1014af4c0eef4db81298dfcda9ae722e29fca04
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2023-11-30 20:41:24 +0530

    Time: 17 ms (74.03%), Space: 11.4 MB (43.18%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -4,17 +4,14 @@ public:
         string ans="";
         int i=0;
         while(i<s.length()){
-            if(ans.length()>0){
-                if(ans[ans.length()-1] == s[i]){
+            
+                if(ans.length()>0 && ans[ans.length()-1] == s[i]){
                     ans.pop_back();
                 }
                 else{
                     ans.push_back(s[i]);
                 }
-            }
-            else{
-                ans.push_back(s[i]);
-            }
+
             i++;
         }
         return ans;
