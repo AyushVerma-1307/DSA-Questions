56fc636bfb21b5eb82329720e289fdf05e3d932f
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2023-12-03 22:31:17 +0530

    Time: 4 ms (74.88%), Space: 7.6 MB (27.19%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,21 +1,47 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        int hash[256]={0};
-        bool tmapped[256]={0};
+        unordered_map<char,int> mp1;
+        unordered_map<char,bool> mp2;
         
         for(int i=0;i<s.size();i++){
-            if(hash[s[i]] == 0 && !tmapped[t[i]]){
-                hash[s[i]] = t[i];
-                tmapped[t[i]] = 1;
+            if(mp1[s[i]] == 0 && mp2[t[i]] == 0){
+                mp1[s[i]] = t[i];
+                mp2[t[i]] = 1;
             }
         }
         for(int i=0;i<s.size();i++){
-            if(hash[s[i]] != t[i]){
+            if(mp1[s[i]] != t[i]){
                 return false;
             }
         }
         return true;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        //         int hash[256]={0};
+//         bool tmapped[256]={0};
+        
+//         for(int i=0;i<s.size();i++){
+//             if(hash[s[i]] == 0 && !tmapped[t[i]]){
+//                 hash[s[i]] = t[i];
+//                 tmapped[t[i]] = 1;
+//             }
+//         }
+//         for(int i=0;i<s.size();i++){
+//             if(hash[s[i]] != t[i]){
+//                 return false;
+//             }
+//         }
+//         return true;
         
     }
 };
