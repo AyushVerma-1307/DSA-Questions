b84ce9ef90c29562a81429f97b44842340caac7b
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-01-13 22:59:50 +0530

    Time: 4 ms (61.34%), Space: 8.5 MB (40.22%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+    int x = 0;
+    vector<string> ans;
+    bool first = false;
+    for (int i = 0; i <= s.length(); i++) {
+        if ((i < s.length() && isalpha(s[i])) || (i < s.length() && isdigit(s[i]))) {
+            if (!first) {
+                first = true;
+                x = i;
+            }
+        } else if (i < s.length() && s[i] == ' ' && first) {
+            first = false;
+            string str = "";
+            for (int j = x; j < i; j++) {
+                str += s[j];
+            }
+            ans.push_back(str);
+        }
+    }
+
+    if (first) { // Handle the last word if it exists
+        string str = "";
+        for (int j = x; j < s.length(); j++) {
+            str += s[j];
+        }
+        ans.push_back(str);
+    }
+
+    string result;
+    for (int i = ans.size() - 1; i >= 0; i--) {
+        result += ans[i];
+        if (i > 0) {
+            result += " ";
+        }
+    }
+    return result;
+}
+};
