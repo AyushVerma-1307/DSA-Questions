87875dd2e4ec44d6a8400f356cca767013f90356
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-01-14 13:52:32 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (45.86%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
     string reverseWords(string s) {
-    int x = 0;
-    vector<string> ans;
-    bool first = false;
-    for (int i = 0; i <= s.length(); i++) {
-        if ((i < s.length() && isalpha(s[i])) || (i < s.length() && isdigit(s[i]))) {
-            if (!first) {
-                first = true;
-                x = i;
+        int x = 0;
+        vector<string> ans;
+        bool first = false;
+        for (int i = 0; i <= s.length(); i++) {
+            if (isalpha(s[i]) ||isdigit(s[i])) {
+                if (!first) {
+                    first = true;
+                    x = i;
+                }
+            } else if (s[i] == ' ' && first) {
+                first = false;
+                string str = "";
+                for (int j = x; j < i; j++) {
+                    str += s[j];
+                }
+                ans.push_back(str);
             }
-        } else if (i < s.length() && s[i] == ' ' && first) {
-            first = false;
+        }
+
+        if (first) { // Handle the last word if it exists
             string str = "";
-            for (int j = x; j < i; j++) {
+            for (int j = x; j < s.length(); j++) {
                 str += s[j];
             }
             ans.push_back(str);
         }
-    }
 
-    if (first) { // Handle the last word if it exists
-        string str = "";
-        for (int j = x; j < s.length(); j++) {
-            str += s[j];
+        string result;
+        for (int i = ans.size() - 1; i >= 0; i--) {
+            result += ans[i];
+            if (i > 0) {
+                result += " ";
+            }
         }
-        ans.push_back(str);
+        return result;
     }
-
-    string result;
-    for (int i = ans.size() - 1; i >= 0; i--) {
-        result += ans[i];
-        if (i > 0) {
-            result += " ";
-        }
-    }
-    return result;
-}
 };
