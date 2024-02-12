fad7c2f0ab77b71f65ff4222f344fe239ea6c28a
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-02-12 16:06:32 +0530

    Time: 507 ms (97.73%), Space: 298.3 MB (16.00%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -12,7 +12,7 @@ class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
         if(head == NULL || head-> next == NULL){
-            return nullptr;
+            return NULL;
         }
         ListNode* slow = head;
         ListNode* fast = head;
