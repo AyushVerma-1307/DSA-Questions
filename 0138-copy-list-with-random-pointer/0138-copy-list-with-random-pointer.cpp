6d9cc1cdedd83d3aae6080bf504ba1760c6e6dc7
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-02-14 13:21:29 +0530

    Time: 5 ms (71.06%), Space: 12.9 MB (31.67%) - LeetHub

---
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -0,0 +1,51 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+
+class Solution {
+public:
+    Node* copyRandomList(Node* head) {
+        if(!head)return head;
+        
+        //clone the LL
+        Node* temp = head;
+        while(temp){
+            Node* newNode = new Node(temp->val);
+            newNode -> next = temp->next;
+            temp->next = newNode;
+            temp = temp -> next -> next;
+        }
+        
+        //Assign random links of newnode A' with help of old node A
+        temp = head;
+        while(temp){
+            Node* newNode = temp ->next;
+            newNode -> random =temp -> random ? temp -> random -> next : nullptr; 
+            temp = temp -> next -> next;
+        }
+        //step 3: Detach A' from A
+        temp = head;
+        Node* clonedHead = temp -> next;
+        while(temp){
+            Node* clonedHead = temp -> next;
+            temp -> next = temp -> next -> next;
+            if(clonedHead -> next != NULL){
+                clonedHead -> next = clonedHead -> next -> next;
+            }
+            temp = temp -> next;
+        }
+        return clonedHead;
+    }
+};
