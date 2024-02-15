835c6837b9b9b9f6ef1782237b87a65805a7e7cf
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-02-15 17:55:33 +0530

    Time: 154 ms (93.17%), Space: 115.7 MB (57.79%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -12,43 +12,57 @@ class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
         vector<int> ans = {-1,-1};
-        
+
+        // Check if head or its next two nodes are null, if yes return ans
         if(!head || !head->next || !head->next->next){
             return ans;
         }
+
+        // Initialize pointers
         ListNode* prev = head;
-        ListNode* curr = head -> next;
-        ListNode* nxt = head -> next -> next;
-        
+        ListNode* curr = head->next;
+        ListNode* nxt = head->next->next;
+
+        // Initialize variables to track critical points and distances
         int firstCp = -1;
         int lastCp = -1;
-        
         int minDis = INT_MAX;
-        int i=1;
+        int i = 1;
+
+        // Loop until nxt is not null
         while(nxt){
+            // Check if curr is a critical point
             bool cp = ((prev->val < curr->val && curr->val > nxt->val) || 
                        (prev->val > curr->val && curr->val < nxt->val ))
                         ? true : false;
+
+            // If it's a critical point and it's the first one encountered
             if(cp && firstCp == -1){
                 firstCp = i;
                 lastCp = i;
             }
+            // If it's a critical point and it's not the first one encountered
             else if(cp){
                 minDis = min(minDis, i - lastCp);
                 lastCp = i;
             }
+
+            // Update pointers and index
             prev = curr;
             curr = nxt;
             nxt = nxt->next;
             i++;
         }
+
+        // If there's only one critical point, return ans
         if(lastCp == firstCp){
             return ans;
         }
+        // Otherwise, update ans with the distances
         else{
             ans[0] = minDis;
             ans[1] = lastCp - firstCp;
         }
-        return ans;
+        return ans; 
     }
 };
