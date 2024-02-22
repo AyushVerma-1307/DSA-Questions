22275987f6824e15fe8cd0fc0fcc9ea6b68af496
Ayush Verma <104214949+AyushVerma-1307@users.noreply.github.com>
2024-02-22 20:29:47 +0530

    Time: 50 ms (20.18%), Space: 26.7 MB (5.07%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,89 @@
+class Solution {
+public:
+    vector<int> nextSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=n-1; i>=0 ; i--) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans;
+    }
+    
+    vector<int> prevSmallerElement(vector<int> arr, int n) {
+        stack<int> s;
+        s.push(-1);
+        vector<int> ans(n);
+
+        for(int i=0; i<n; i++) {
+            int curr = arr[i];
+            while(s.top() != -1 && arr[s.top()] >= curr)
+            {
+                s.pop();
+            }
+            //ans is stack ka top
+            ans[i] = s.top();
+            s.push(i);
+        }
+        return ans; 
+    }
+    int largestRectangleArea(vector<int>& heights) {
+        int n= heights.size();
+        
+        vector<int> next(n);
+        next = nextSmallerElement(heights, n);
+            
+        vector<int> prev(n);
+        prev = prevSmallerElement(heights, n);
+        
+        int area = INT_MIN;
+        for(int i=0; i<n; i++) {
+            int l = heights[i];
+            
+            if(next[i] == -1) {
+                next[i] = n;
+            }
+             int b = next[i] - prev[i] - 1;
+            int newArea = l*b;
+            area = max(area, newArea);
+        }
+        return area;
+    }
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        vector<vector<int>> v;
+        int n=matrix.size();
+        int m=matrix[0].size();
+        
+        for(int i=0;i<n;++i){
+            vector<int> t;
+            for(int j=0;j<m;++j){
+                t.push_back(matrix[i][j] -'0');
+            }
+            v.push_back(t);
+        }
+        
+        int area = largestRectangleArea(v[0]);
+        
+        for(int i=1;i<n;++i){
+            for(int j=0;j<m;++j){
+                //update current row with previous value
+                if(v[i][j]){
+                    v[i][j] += v[i-1][j];
+                }
+                else{
+                    v[i][j] = 0;
+                }
+            }
+            area = max(area,largestRectangleArea(v[i]));
+        }
+        return area;
+    }
+};
